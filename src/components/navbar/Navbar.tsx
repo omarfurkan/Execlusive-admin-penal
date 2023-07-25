0d97e6a5324cf0d367985b28be4558cd52728867
omarfurkan <omarfurkan92@gmail.com>
2023-07-25 13:05:13 +0600

    some img in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,31 @@
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>Execlusive</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notification.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://i.ibb.co/T22W4G3/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-ta.jpg"
+            alt=""
+          />
+          <span>Jane</span>
+        </div>
+        <img src="" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
