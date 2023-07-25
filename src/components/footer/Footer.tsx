7564220a363d05b7f09315f9d5ffb6a08a1c45cd
omarfurkan <omarfurkan92@gmail.com>
2023-07-25 14:20:33 +0600

    partial sidebar with design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <span>Exeslusiv</span>
+      <span> React Admin Dashbord</span>
+    </div>
+  );
 };
 
 export default Footer;
